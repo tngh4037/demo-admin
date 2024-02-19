6cee3de09e824f5afb0d0eb03ff0ca5c00ef5f35
tngh4037 <tngh4037@gmail.com>
2024-02-20 00:53:20 +0900

    feat: sidebar 메뉴 활성화 조건에 여러 url 경로가 있는 경우 처리될 수 있도록 적용

--- src/main/java/com/example/demo/admin/domain/home/controller/HomeController.java
+++ src/main/java/com/example/demo/admin/domain/home/controller/HomeController.java
@@ -2,28 +2,21 @@ package com.example.demo.admin.domain.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
+    /**
+     * 화면 구성을 위한 샘플 페이지
+     * - pageMode(search): 조회 리스트 페이지
+     * - pageMode(detail):조회 상세 / 등록 / 수정 페이지
+     */
     @GetMapping("/")
-    public String getHome() {
+    public String getHome(@RequestParam(value = "pageMode", required = false, defaultValue = "search") String pageMode) {
+        if (pageMode.equals("detail")) {
+            return "sample/sampleDetail";
+        }
         return "/sample/sampleList";
-    }
-
-    /**
-     * (화면 구성을 위한 샘플 페이지) list page
-     */
-    @GetMapping("/sample/list")
-    public String getSampleList() {
-        return "sample/sampleList";
-    }
-
-    /**
-     * (화면 구성을 위한 샘플 페이지) detail page
-     */
-    @GetMapping("/sample/detail")
-    public String getSampleDetail() {
-        return "sample/sampleDetail";
     }
 }
---
+++ src/main/resources/static/js/common/demo-sidebar.js
@@ -0,0 +1,28 @@
+$(function() {
+  DemoSideBarModule._setActiveLink()
+})
+
+const DemoSideBarModule = (function() {
+  const settings = {
+    $a: {
+      link: $('a.nav-link[data-multi-page]')
+    }
+  }
+
+  /**
+   * (참고) coreui.bundle.min.js > _setActiveLink
+   */
+  const _setActiveLink = function() {
+    settings.$a.link.each(function() {
+      if (window.location.href !== $(this).prop('href') &&
+            window.location.href.startsWith($(this).prop('href'))) {
+        $(this).closest('li.nav-group').addClass('show').attr('aria-expanded', true)
+        $(this).addClass('active')
+      }
+    })
+  }
+
+  return {
+    _setActiveLink: _setActiveLink
+  }
+})()
